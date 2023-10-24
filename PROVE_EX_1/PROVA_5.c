fc531a41cf4e99f01a5c83310331735d8d57640c
EmmeRaid <bellomatteo@icloud.com>
2023-10-24 11:20:33 +0200

    PRIMI ESERCIZI 	new file:   PROVE_EX_1/BIMBUMBAM.c 	new file:   PROVE_EX_1/PRODOTTO.c 	new file:   PROVE_EX_1/PROVA_1.c 	new file:   PROVE_EX_1/PROVA_2.c 	new file:   PROVE_EX_1/PROVA_3.c 	new file:   PROVE_EX_1/PROVA_4.c 	new file:   PROVE_EX_1/PROVA_5.c 	new file:   PROVE_EX_1/a.out
    	deleted:    BIMBUMBAM.c
    	deleted:    PRODOTTO.c
    	deleted:    PROVA_1.c
    	deleted:    PROVA_2.c
    	deleted:    PROVA_3.c
    	deleted:    PROVA_4.c#
    	deleted:    PROVA_5.c
    	deleted:    a.out

---
+++ PROVE_EX_1/PROVA_5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int numero;
+
+    printf("Inserisci un numero intero: ");
+    scanf("%d", &numero);
+
+    printf("I divisori di %d sono:\n", numero);
+
+    for (int i = 1; i <= numero; i++)
+    {
+        if (numero % i == 0)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
